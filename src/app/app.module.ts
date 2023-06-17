1f8f509d50d306a205642b097831cd9f1d5501bf
JesusOyola <jesus.oyola@interatica.com>
2023-06-17 14:16:31 -0300

    Generacion de rutas, vistas y formulario en el frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListProductsComponent,
+    AddEditProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/list-products/list-products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+
+@Component({
+  selector: 'app-list-products',
+  templateUrl: './list-products.component.html',
+  styleUrls: ['./list-products.component.css'],
+})
+export class ListProductsComponent implements OnInit {
+  listProducts: Product[] = [
+    {
+      id: 1,
+      name: 'Coca cola',
+      description: ' Bebida con azucar',
+      price: 4,
+      stock: 200,
+    },
+    {
+      id: 2,
+      name: 'Corona',
+      description: ' Bebida con alcohol',
+      price: 5,
+      stock: 300,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
